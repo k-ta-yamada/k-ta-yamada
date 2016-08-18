ee2b80fdea88b95983d1d60fc5b7af20f1c6d22e
k-ta-yamada <key.luvless@gmail.com>
2016-08-18 11:46:05 +0900

    [mod] error handling

--- public/js/rubygems.js
+++ public/js/rubygems.js
@@ -1,19 +1,26 @@
 angular.module('myApp', ['angular-loading-bar', 'ngAnimate'])
   .controller('myController', ['$scope', '$http', function($scope, $http) {
     $scope.getGemList = () => {
+      $scope.isError = false;
       $http.get('/rubygems.json')
         .success(function(data) {
           // console.table(data);
           $scope.data_set = data;
           $scope.generateChart('renc');
           $scope.generateChart('tee_logger');
+        })
+        .error(function(data, status, headers, config) {
+          $scope.errors.push({status: status, function: 'getGemList'});
+          $scope.isError = true;
         });
     };
 
+    $scope.errors = [];
     $scope.chart = {};
 
     $scope.generateChart = (name) => {
       // console.log(name);
+      $scope.isError = false;
       $http.get('/rubygems/' + name)
         .success(function(data) {
           var chart = c3.generate({
@@ -25,19 +32,20 @@ angular.module('myApp', ['angular-loading-bar', 'ngAnimate'])
               x: 'number',
               labels: true,
               // types: { downloads_count: 'area' },
-              names: { downloads_count: 'downloads/version' },
+              names: { downloads_count: 'downloads/version' }
             },
             axis: { x: { type: 'category' }, },
             grid: { y: { show: true } },
             line: { connectNull: true },
             area: { zerobased: false },
-            bar:  { zerobased: false, width: { ratio: 0.4 } },
+            bar:  { zerobased: false, width: { ratio: 0.4 } }
           });
 
-          $scope.chart[name] = {
-            isShow: true,
-            chart: chart,
-          };
+          $scope.chart[name] = { isShow: true, chart: chart };
+        })
+        .error(function(data, status, headers, config) {
+          $scope.errors.push({status: status, function: 'generateChart(' + name + ')'});
+          $scope.isError = true;
         });
     };
 
