12878be02cbc4f4078eb6568bb525d7e6df972a8
k-ta-yamada <key.luvless@gmail.com>
2018-10-24 22:02:36 +0900

    [add] nav > menu: plank challenge

--- sources/vue/common/menu.vue
+++ sources/vue/common/menu.vue
@@ -3,6 +3,7 @@ export default [
   { title: "prof",     link: "/prof" },
   { title: "rubygems", link: "/rubygems" },
   { title: "commit",   link: "/repo" },
+  { title: "30 day plank challenge",   link: "/30day_plank_challenge" },
   // { title: "upload",   link: "/img_upload" },
 ]
 </script>
