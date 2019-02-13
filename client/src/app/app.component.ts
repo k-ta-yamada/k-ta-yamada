6cc1886d6e876ed84b071f71d48dc6b29a8a8085
k-ta-yamada <key.luvless@gmail.com>
2019-02-13 12:03:06 +0900

    scroll to top on route click
    ref: https://stackoverflow.com/questions/48048299/angular-5-scroll-to-top-on-every-route-click

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'k-ta-yamada';
+
+  onActive(event) {
+    window.scroll(0, 0);
+  }
+
 }
