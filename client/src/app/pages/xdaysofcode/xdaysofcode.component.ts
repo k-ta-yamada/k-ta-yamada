e66dbec1f2e7bd1ad1be1aca5794e0bc07721d53
k-ta-yamada <key.luvless@gmail.com>
2019-02-13 17:19:03 +0900

    [wip][upd] /xdaysofcode

--- client/src/app/pages/xdaysofcode/xdaysofcode.component.ts
+++ client/src/app/pages/xdaysofcode/xdaysofcode.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-xdaysofcode',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class XdaysofcodeComponent implements OnInit {
 
-  constructor() { }
+  html: any;
+
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.http.get('api/100daysofcode').subscribe(r => {
+      console.log(r);
+      this.html = this.sanitizer.bypassSecurityTrustHtml(r.toString());
+    });
   }
 
 }
